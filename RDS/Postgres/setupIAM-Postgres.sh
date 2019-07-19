ea040ea95f192f9afeea896aede656c917a568ad
Bruno Carvalho <bruno580@gmail.com>
2019-07-20 08:55:12 +1000

    Removed create role functionality and whitelisting of root user

--- RDS/Postgres/setupIAM-Postgres.sh
+++ RDS/Postgres/setupIAM-Postgres.sh
@@ -10,13 +10,31 @@
 # BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under the License.
 
+## Documentation related to this script:
+# https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.IAMDBAuth.html#UsingWithRDS.IAMDBAuth.Availability
+
 ## Functions ##
+usage(){
+    cat << EOF
+Usage: 
+./setupIAM-Postgres.sh <AWS CLI Profile [optional]>
+EOF
+}
 securityDisclaimer(){
     # This function just lets the user know what will be done so he can cancel before doing anything if he wants.
     cat << EOF
-Info: Executing this script will:
+This script helps setting up the IAM Policy and Role to allow RDS Postgres Authentication using IAM.
+
+Versions supported:
+RDS Postgresql 9.5.13 or higher
+RDS Postgresql 9.6.9 or higher
+RDS Postgresql 10.4 or higher
+Aurora Postgresql 9.6.9 or higher
+Aurora Postgresql 10.4 or higher
+
+Security Disclaimer - Executing this script will:
     1. Query your account ID from AWS CLI.
-    2. Create IAM Policy and Role.
+    2. Create IAM Policy and attach to the desired IAM User.
     3. Download SSL certificate to this machine.
     4. Create parameter file on ${HOME}
 
@@ -26,16 +44,16 @@ EOF
 }
 getAccountID(){
     # This requires AWS CLI to be properly configured!
-    ACC_ID="$(aws sts get-caller-identity --query "[Account]" --output text)"
+    ACC_ID="$(aws sts get-caller-identity --query "[Account]" --output text --profile ${PROFILE})"
     echo "Account ID: ${ACC_ID}"
     return 0
 }
 loadVariables() {
-    read -p "Role Name [IAMDBAuthPostgresRole]: " ROLE_NAME; ROLE_NAME=${ROLE_NAME:-IAMDBAuthPostgresRole}
     read -p "Policy Name [IAMDBAuthPostgresPolicy]: " POLICY_NAME; POLICY_NAME=${POLICY_NAME:-IAMDBAuthPostgresPolicy}
     read -p "Region [us-east-1]: " REGION; REGION=${REGION:-us-east-1}
     read -p "DB Resource ID [*]: " DB_RES_ID; DB_RES_ID=${DB_RES_ID:-*}
     read -p "IAM User: " IAM_USER
+    read -p "DB User: [${IAM_USER}] " DB_USER; if [[ -z $DB_USER ]]; then DB_USER=${IAM_USER}; fi
     return 0
 }
 createJSONPolicy() {
@@ -49,7 +67,7 @@ cat << EOF > rds-iam-policy.json
                 "rds-db:connect"
             ],
             "Resource": [
-                "arn:aws:rds:${REGION}:${ACC_ID}:dbuser:${DB_RES_ID}/${IAM_USER}"
+                "arn:aws:rds:${REGION}:${ACC_ID}:dbuser:${DB_RES_ID}/${DB_USER}"
             ]
         }
     ]
@@ -58,30 +76,12 @@ EOF
 POLICY_ARN="arn:aws:iam::${ACC_ID}:policy/${POLICY_NAME}"
 return 0
 }
-createJSONRole() {
-cat << EOF > rds-iam-role.json
-{
-  "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Allow",
-    "Principal": { "AWS": "arn:aws:iam::${ACC_ID}:root" },
-    "Action": "sts:AssumeRole"
-  }
-}
-EOF
-ROLE_ARN="arn:aws:iam::${ACC_ID}:role/${ROLE_NAME}"
-return 0
-}
 createIAMPolicy() {
-    aws iam create-policy --policy-name ${POLICY_NAME} --policy-document file://rds-iam-policy.json
+    aws iam create-policy --policy-name ${POLICY_NAME} --policy-document file://rds-iam-policy.json --profile ${PROFILE}
     return 0
 }
-createIAMRole() {
-    aws iam create-role --role-name ${ROLE_NAME} --assume-role-policy-document file://rds-iam-role.json
-    return 0
-}
-attachIAMPolicyToRole(){
-    aws iam attach-role-policy --policy-arn ${POLICY_ARN} --role-name ${ROLE_NAME}
+attachIAMPolicyToUser(){
+    aws iam attach-user-policy --policy-arn ${POLICY_ARN} --user-name ${IAM_USER} --profile ${PROFILE}
     return 0
 }
 getSSLCertificate(){
@@ -94,7 +94,12 @@ getSSLCertificate(){
     fi
 }
 createParameterFile() {
-if [[ -f ~/.pg_${IAM_USER} ]]; then exit 0; fi
+if [[ -f ~/.pg_${IAM_USER} ]]; 
+then 
+    echo "Configuration file already exists, run the command below to use IAM authentication: " 
+    echo ". ~/.pg_${IAM_USER}" 
+    exit 0
+else
     echo "Please provide database details to create the parameter file that will be used to generate the authentication token."
     read -p "Database Endpoint: " RDSHOST
     read -p "Database Port: [5432] " RDSPORT; RDSPORT=${RDSPORT:-5432}
@@ -104,11 +109,12 @@ export RDSHOST="${RDSHOST}"
 export RDSPORT="${RDSPORT}"
 export RDSDB="${RDSDB}"
 export REGION="${REGION}"
-export IAM_USER="${IAM_USER}"
-export CONN="psql \"host=$RDSHOST dbname=$RDSDB user=$IAM_USER sslrootcert=rds-combined-ca-bundle.pem sslmode=verify-full\""
+export DB_USER="${DB_USER}"
+export CONN="psql \"host=$RDSHOST dbname=$RDSDB user=$DB_USER sslrootcert=rds-combined-ca-bundle.pem sslmode=verify-full\""
 EOF
-echo "export PGPASSWORD=\"\$(aws rds generate-db-auth-token --hostname \$RDSHOST --port \$RDSPORT --region \$REGION --username \$IAM_USER)\"" >> ~/.pg_${IAM_USER}
+echo "export PGPASSWORD=\"\$(aws rds generate-db-auth-token --hostname \$RDSHOST --port \$RDSPORT --region \$REGION --username \$DB_USER)\"" >> ~/.pg_${IAM_USER}
 echo "echo \$CONN" >> ~/.pg_${IAM_USER} 
+fi
 return 0
 }
 validateSettings(){
@@ -116,18 +122,20 @@ validateSettings(){
     if [[ -z $IAM_USER ]]; then echo "No IAM user was entered. Rerun the script and enter the IAM User." && exit 1; fi
 }
 
+## Sets the profile to be used for CLI and displays usage if user types -h or --help
+if [[ -z $1 ]]; then PROFILE="default"; else PROFILE=${1}; fi
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then usage && exit 0; fi
+
 ## MAIN Workflow ##
 securityDisclaimer
 getAccountID
 loadVariables
 createJSONPolicy
-createJSONRole
 validateSettings
 createIAMPolicy
-createIAMRole
-attachIAMPolicyToRole
-createParameterFile
+attachIAMPolicyToUser
 getSSLCertificate
+createParameterFile
 . ~/.pg_${IAM_USER} > /dev/null
 if [[ -z $PGPASSWORD ]] 
 then 
@@ -139,11 +147,11 @@ else
     echo ''
     echo "*** Environment configured successfully ***"
     echo ''
-    echo "Make sure you add the IAM users to be authenticated to the role ${ROLE_NAME}."
+    echo "You may add the policy ${POLICY_NAME} to users and roles that will connect to the database/s."
     echo ''
     echo "Please connect with the master user to your database and execute the following commands: "
-    echo "create user ${IAM_USER} with login;"
-    echo "grant rds_iam to ${IAM_USER};"
+    echo "create user ${DB_USER} with login;"
+    echo "grant rds_iam to ${DB_USER};"
     echo ''
     echo "You can connect to the database now with the following connection string: "
     echo "${CONN}"
